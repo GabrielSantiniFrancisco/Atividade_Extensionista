4dc6932668848f9f4b3e91ad43fce55dfa92a1dd
GabrielSantiniFrancisco <168279921+GabrielSantiniFrancisco@users.noreply.github.com>
2024-10-02 15:22:49 -0300

    Create anomaly_api.py

---
+++ anomaly_api.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+
+#
+# author : Gabriel Francisco
+# email  : gabriel.francisco@ibm.com
+# date   : 01/10/2024
+# version: 0.1b
+
+
+##########################
+# SETUP INICIAL
+##########################
+
+from flask import Flask, request, jsonify
+import joblib, smtplib, threading, time, configparser
+from email.mime.text import MIMEText
+
+##########################
+# FUNÇÕES
+##########################
+
+# Função para ler configurações
+def read_config() -> configparser.ConfigParser:
+    '''
+    Função para ler as configurações do arquivo 'conf.cfg' utilizando a classe ConfigParser.
+
+    Parâmetros:
+    Nenhum
+
+    Retorna:
+    configparser.ConfigParser
+        Um objeto ConfigParser contendo as configurações lidas do arquivo 'conf.cfg'.
+    '''
+    config = configparser.ConfigParser()  # Cria um objeto ConfigParser
+    config.read('conf.cfg')  # Lê o arquivo de configuração 'conf.cfg'
+    return config  # Retorna o objeto ConfigParser contendo as configurações lidas
+
+# Função para atualizar configurações on-the-fly
+def update_config() -> None:
+    '''
+    Função para atualizar as configurações do sistema dinamicamente.
+
+    Parâmetros:
+    Nenhum
+
+    Retorna:
+    None
+        Não retorna nenhum valor. A função atualiza a variável global 'global_config' com as novas configurações.
+    '''
+    global global_config  # Declara a variável global 'global_config'
+    while True:
+        global_config = read_config()  # Lê e atualiza as configurações
+        time.sleep(60)  # Pausa de 60 segundos antes de atualizar novamente
+
+
+# Função para enviar alerta
+def send_alert(message, equipamento) -> None:
+    '''
+    Função para enviar um alerta por e-mail com a mensagem e o equipamento especificado.
+
+    Parâmetros:
+    message : str
+        A mensagem a ser enviada no alerta.
+    equipamento : str
+        O nome do equipamento relacionado à mensagem.
+
+    Retorna:
+    None
+        Não retorna nenhum valor. A função envia um e-mail com o alerta.
+    '''
+    config = global_config  # Obtém as configurações globais
+    smtp_server = config['email']['smtp_server']  # Servidor SMTP
+    smtp_port = config['email']['smtp_port']  # Porta do servidor SMTP
+    username = config['email']['username']  # Nome de usuário do e-mail
+    password = config['email']['password']  # Senha do e-mail
+    recipient = config['email']['recipient']  # Destinatário do e-mail
+
+    msg = MIMEText(f'{message}\nEquipamento: {equipamento}')  # Cria a mensagem de e-mail
+    msg['Subject'] = 'Alerta de Anomalia'  # Assunto do e-mail
+    msg['From'] = username  # Remetente do e-mail
+    msg['To'] = recipient  # Destinatário do e-mail
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()  # Inicia o TLS para segurança
+        server.login(username, password)  # Faz login no servidor de e-mail
+        server.send_message(msg)  # Envia a mensagem de e-mail
+
+
+# Função para iniciar servidor Flask para cada equipamento
+def start_server(port, equipamento) -> None:
+    '''
+    Função para iniciar um servidor Flask para monitorar cada equipamento específico.
+
+    Parâmetros:
+    port : int
+        A porta na qual o servidor Flask irá rodar.
+    equipamento : str
+        O nome do equipamento que está sendo monitorado.
+
+    Retorna:
+    None
+        Não retorna nenhum valor. A função inicia um servidor Flask que escuta na porta especificada.
+    '''
+    app = Flask(__name__)  # Cria uma instância do Flask
+    model = joblib.load('modelo_lof.pkl')  # Carrega o modelo de machine learning
+
+    @app.route('/predict', methods=['POST'])
+    def predict():
+        data = request.json['data']  # Obtém os dados JSON da requisição
+        prediction = model.predict(data)  # Faz a predição com o modelo
+        if -1 in prediction:  # Detecção de anomalia
+            send_alert('Anomalia detectada no sistema! Verifique imediatamente.', equipamento)
+        return jsonify({'prediction': prediction.tolist()})  # Retorna a predição em formato JSON
+
+    app.run(debug=True, host='0.0.0.0', port=port)  # Inicia o servidor Flask na porta especificada
+
+
+# Função principal para iniciar threads
+def main() -> None:
+    '''
+    Função principal para iniciar threads que atualizam as configurações e iniciam servidores Flask para cada equipamento.
+
+    Parâmetros:
+    Nenhum
+
+    Retorna:
+    None
+        Não retorna nenhum valor. A função inicia threads que lidam com a atualização das configurações e o monitoramento dos equipamentos.
+    '''
+    config_thread = threading.Thread(target=update_config)  # Cria uma thread para atualizar as configurações
+    config_thread.start()  # Inicia a thread de atualização das configurações
+
+    config = global_config  # Obtém as configurações globais
+    ports = [config['server']['port_equipment1'], config['server']['port_equipment2']]  # Define as portas dos servidores Flask
+    equipamentos = ['Equipamento1', 'Equipamento2']  # Define os nomes dos equipamentos
+
+    server_threads = []  # Lista para armazenar as threads dos servidores Flask
+    for port, equipamento in zip(ports, equipamentos):
+        thread = threading.Thread(target=start_server, args=(port, equipamento))  # Cria uma thread para cada servidor Flask
+        server_threads.append(thread)  # Adiciona a thread à lista de threads
+        thread.start()  # Inicia a thread do servidor Flask
+
+    for thread in server_threads:
+        thread.join()  # Aguarda todas as threads dos servidores Flask terminarem
+
+
+##########################
+# VARIAVEIS
+##########################
+global_config = read_config()
+
+# Inicialição da API
+if __name__ == '__main__':
+    main()
